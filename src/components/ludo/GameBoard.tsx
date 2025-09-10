442cf4ece96c7ba69ca65ce667d429a30737c96b
Mushangi P <mushangip0@gmail.com>
2025-09-10 20:59:52 +0000

    remove this diagnol line in this cell , that start from topleft of this

--- src/components/ludo/GameBoard.tsx
+++ src/components/ludo/GameBoard.tsx
@@ -90,13 +90,7 @@ export function GameBoard({ children }: { children: ReactNode }) {
                  return <div className={cn(borderClasses, bgColor, "relative h-full w-full")}></div>;
             }
              if (p(x,y) === START_POSITIONS.green) {
-                return (
-                    <div className={cn(borderClasses, bgColor, "relative h-full w-full")}>
-                        <svg viewBox="0 0 100 100" className="absolute w-full h-full">
-                           <line x1="0" y1="0" x2="100" y2="100" stroke="black" strokeWidth="2" />
-                        </svg>
-                    </div>
-                );
+                return <div className={cn(borderClasses, bgColor, "relative h-full w-full")}></div>;
             }
             if (p(x, y) === START_POSITIONS.yellow) {
               return (
