7356faa8049ceab5f9beef9fe4330d0235cdc6dd
Mushangi P <mushangip0@gmail.com>
2025-10-19 15:02:50 +0000

    return to when i previously had a 2d dice . that roll .

--- next.config.ts
+++ next.config.ts
@@ -24,19 +24,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(glb|gltf)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/_next/static/images',
-          outputPath: 'static/images/',
-        },
-      },
-    });
-    return config;
-  }
 };
 
 export default nextConfig;
