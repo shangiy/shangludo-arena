32bd2ee5d47885d58b99a58e9030f544f52f135e
Mushangi P <mushangip0@gmail.com>
2025-11-22 11:57:21 +0000

    the orientation of the ludo triangle red/ blue do not look likr the imag

--- src/components/ludo/GameBoard.tsx
+++ src/components/ludo/GameBoard.tsx
@@ -92,23 +92,23 @@ export function GameBoard({
         let triangle1 = '', triangle2 = '', color1 = '', color2 = '';
 
         if (x === 6 && y === 6) { // top-left
-            triangle1 = '0,0 0,100 100,0';
-            triangle2 = '100,100 100,0 0,100';
+            triangle1 = '0,0 100,0 0,100';
+            triangle2 = '100,0 100,100 0,100';
             color1 = 'fill-red-500';
             color2 = 'fill-green-500';
         } else if (x === 8 && y === 6) { // top-right
-            triangle1 = '0,0 100,0 0,100';
-            triangle2 = '100,0 100,100 0,100';
-            color1 = 'fill-green-500';
-            color2 = 'fill-yellow-400';
+            triangle1 = '0,0 100,0 100,100';
+            triangle2 = '0,0 0,100 100,100';
+            color1 = 'fill-yellow-400';
+            color2 = 'fill-green-500';
         } else if (x === 6 && y === 8) { // bottom-left
-            triangle1 = '0,0 100,100 0,100';
-            triangle2 = '0,0 100,0 100,100';
-            color1 = 'fill-red-500';
-            color2 = 'fill-blue-500';
-        } else if (x === 8 && y === 8) { // bottom-right
             triangle1 = '0,100 100,100 100,0';
             triangle2 = '0,0 0,100 100,0';
+            color1 = 'fill-blue-500';
+            color2 = 'fill-red-500';
+        } else if (x === 8 && y === 8) { // bottom-right
+            triangle1 = '0,0 100,100 0,100';
+            triangle2 = '0,0 100,0 100,100';
             color1 = 'fill-blue-500';
             color2 = 'fill-yellow-400';
         }
