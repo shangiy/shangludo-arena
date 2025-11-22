a1746bd1dfd56fb7bdbd876a298bf6366facc1db
Mushangi P <mushangip0@gmail.com>
2025-11-22 14:10:05 +0000

    counting from Topleft of the gameboard only and only this cell in the ro

--- src/components/ludo/GameBoard.tsx
+++ src/components/ludo/GameBoard.tsx
@@ -89,8 +89,8 @@ export function GameBoard({
         let triangle1 = '', triangle2 = '', color1 = '', color2 = '';
         
         if (x === 6 && y === 6) { // top-left
-            triangle1 = '0,0 100,0 100,100';
-            triangle2 = '0,0 0,100 100,100';
+            triangle1 = '0,0 100,0 0,100';
+            triangle2 = '100,0 100,100 0,100';
             color1 = 'fill-green-500';
             color2 = 'fill-red-500';
         } else if (x === 8 && y === 6) { // top-right (Row 7, Col 9)
@@ -99,15 +99,15 @@ export function GameBoard({
             color1 = 'fill-green-500';
             color2 = 'fill-yellow-400';
         } else if (x === 6 && y === 8) { // bottom-left (Row 9, Col 7)
-            triangle1 = '0,100 100,100 0,0'; // bottom-left triangle (blue)
-            triangle2 = '0,0 100,0 100,100'; // top-right triangle (red)
-            color1 = 'fill-blue-500';
-            color2 = 'fill-red-500';
+            triangle1 = '0,0 100,0 100,100';
+            triangle2 = '0,0 0,100 100,100';
+            color1 = 'fill-red-500';
+            color2 = 'fill-blue-500';
         } else if (x === 8 && y === 8) { // bottom-right
-            triangle1 = '0,100 100,0 100,100';
-            triangle2 = '0,0 100,0 0,100';
-            color1 = 'fill-blue-500';
-            color2 = 'fill-yellow-400';
+            triangle1 = '0,0 100,0 0,100';
+            triangle2 = '0,100 100,0 100,100';
+            color1 = 'fill-yellow-400';
+            color2 = 'fill-blue-500';
         }
 
 
@@ -125,9 +125,9 @@ export function GameBoard({
     if (x === 7 && y === 7) {
       return (
         <div className={cn(borderClasses, 'bg-white relative')}>
-             <svg width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="none">
-                <polygon points="0,0 100,100 0,100" className="fill-red-500" />
+            <svg width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="none">
                 <polygon points="0,0 100,0 100,100" className="fill-green-500" />
+                <polygon points="0,0 0,100 100,100" className="fill-red-500" />
             </svg>
         </div>
       );
