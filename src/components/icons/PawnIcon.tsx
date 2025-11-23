d519a8d5a77209355fb516e437839b2b2c3ff69b
Mushangi P <mushangip0@gmail.com>
2025-11-23 00:49:28 +0000

    since this is a mini player (pawn) of green player , make this TO the  A

--- src/components/icons/PawnIcon.tsx
+++ src/components/icons/PawnIcon.tsx
@@ -5,16 +5,17 @@ import type { SVGProps } from "react";
 
 export function PawnIcon({color, ...props}: SVGProps<SVGSVGElement> & {color: PlayerColor}) {
     const PAWN_COLORS: Record<PlayerColor, string> = {
-        red: 'text-red-500',
-        green: 'text-green-500',
-        yellow: 'text-yellow-400',
-        blue: 'text-blue-500',
+        red: 'fill-red-500',
+        green: 'fill-green-500',
+        yellow: 'fill-yellow-400',
+        blue: 'fill-blue-500',
     }
     return (
-        <svg viewBox="0 0 24 24" fill="currentColor" className={cn("h-5 w-5", PAWN_COLORS[color])} {...props}>
-            <path d="M12 2C9.243 2 7 4.243 7 7c0 2.757 2.243 5 5 5s5-2.243 5-5c0-2.757-2.243-5-5-5z"></path><path d="M12 14c-4.418 0-8 3.582-8 8h16c0-4.418-3.582-8-8-8z"></path>
+        <svg viewBox="0 0 24 24" {...props}>
+            <circle cx="12" cy="12" r="11" fill="black" />
+            <g className={cn(PAWN_COLORS[color])}>
+                <path d="M12 2C9.243 2 7 4.243 7 7c0 2.757 2.243 5 5 5s5-2.243 5-5c0-2.757-2.243-5-5-5z"></path><path d="M12 14c-4.418 0-8 3.582-8 8h16c0-4.418-3.582-8-8-8z"></path>
+            </g>
         </svg>
     )
 };
-
-    
